eea85267e0bfa1fb40eca06a5e60b800cc2d867b
Konstantinos Vasileiou <hello@vsl.io>
2024-09-25 18:36:02 +0300

    updates header with link to cart, lil' bit of cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,37 @@
 <script setup lang="ts">
+import CartIcon from '@/components/icons/CartIcon.vue'
 import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/">👋 e-μπάι</RouterLink>
+      <RouterLink to="/cart"><CartIcon /></RouterLink>
+    </nav>
   </header>
 
   <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 32px;
+  padding: 24px 0;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+a {
+  font-weight: 700;
+  padding: 0 12px;
+  font-size: 1.5rem;
+  line-height: 1;
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  &:first-child {
+    padding-left: 0;
   }
 }
 </style>
