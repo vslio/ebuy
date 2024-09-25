60a62255210ce58b6cd34e5f6c4f9b43c63c8b33
Konstantinos Vasileiou <hello@vsl.io>
2024-09-25 23:05:49 +0300

    adds search icon

---
+++ src/components/icons/SearchIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="size"
+    :height="size"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path
+      d="M17.5 17.5 22 22M20 11a9 9 0 1 0-18 0 9 9 0 0 0 18 0Z"
+      stroke="#2c3e50"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  size?: number
+}
+
+withDefaults(defineProps<Props>(), {
+  size: 32
+})
+</script>
