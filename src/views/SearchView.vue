80e6bc2fa0bb3aea5310e1083507dba5cd3ed459
Konstantinos Vasileiou <hello@vsl.io>
2024-09-26 02:47:08 +0300

    updates the search view template to be more usable

--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -1,34 +1,71 @@
 <template>
-  <h1>Search</h1>
-  <div class="product-grid">
-    <ProductCard v-for="product in products" :key="product.id" :product="product" />
+  <div class="search-view">
+    <div>
+      <h2>You searched for</h2>
+      <h1 class="wavy active" :data-wavy="`${$route.query.term} ${$route.query.term}`">
+        {{ $route.query.term }}
+      </h1>
+    </div>
+    <div v-if="products.length > 0" class="product-grid">
+      <ProductCard v-for="product in products" :key="product.id" :product="product" />
+    </div>
+    <div v-else class="text-center">
+      <span class="emoji">😾</span>
+      <h2>We couldn't find what you're looking for &hellip;</h2>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import ProductCard from '@/components/ProductCard.vue'
 import type { Product } from '@/services/api/handlers'
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, watch } from 'vue'
 import { useRoute } from 'vue-router'
 
+const route = useRoute()
 const products = ref<Product[]>([])
 
 const searchProducts = async () => {
   try {
-    const route = useRoute()
     const response = await fetch(`/api/products/search/${route.query.term}`)
 
     if (!response.ok) {
-      throw new Error('Failed to fetch products')
+      throw new Error('Failed to search for products')
     }
 
     products.value = await response.json()
   } catch (error) {
-    console.error('Error fetching products ->', error)
+    console.error('Error searching products ->', error)
   }
 }
+
+watch(() => route.query.term, searchProducts, { immediate: true })
 
 onMounted(() => {
   searchProducts()
 })
 </script>
+
+<style scoped>
+.search-view {
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+}
+
+h1 {
+  display: inline-block;
+  align-self: flex-start;
+  margin: 0;
+  font-size: 2rem;
+  font-weight: 700;
+}
+
+h2 {
+  margin-bottom: 0;
+}
+
+.emoji {
+  font-size: 3rem;
+}
+</style>
