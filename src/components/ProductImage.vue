e835b8496d7d47be8c4aa9191ce1e07116be9582
Konstantinos Vasileiou <hello@vsl.io>
2024-09-27 00:57:56 +0300

    product image tidy

--- src/components/ProductImage.vue
+++ src/components/ProductImage.vue
@@ -12,35 +12,3 @@ function getImagePath() {
   return new URL(`../assets/images/products/${props.url}`, import.meta.url).href
 }
 </script>
-
-<style scoped>
-.product-card {
-  display: flex;
-  flex-direction: column;
-  gap: 12px;
-}
-
-.product-info {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-}
-
-h2,
-h3 {
-  margin: 0;
-}
-
-h2 {
-  font-size: 1.6rem;
-}
-
-.product-price {
-  font-family: 'PPNeueMachina';
-  font-size: 1.2rem;
-}
-
-img {
-  width: 100%;
-}
-</style>
