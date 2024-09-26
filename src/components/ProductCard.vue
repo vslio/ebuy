ca604d01e143ea01cec9f09fe419266ff4d2c960
Konstantinos Vasileiou <hello@vsl.io>
2024-09-26 18:39:53 +0300

    adds add to cart functionality to product card component, along with a new cart plus icon

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,20 +1,58 @@
 <template>
-  <RouterLink :to="`/product/${product.id}`" :key="product.id" class="product-card">
-    <ProductImage :url="product.image" :alt="`Image of product ${product.name}`" />
+  <div @mouseenter="isHovered = true" @mouseleave="isHovered = false" class="product-card">
+    <RouterLink class="image-container" :to="`/product/${cartProduct.id}`" :key="cartProduct.id">
+      <ProductImage
+        :class="isHovered ? 'zoom' : ''"
+        :url="cartProduct.image"
+        :alt="`Image of product ${cartProduct.name}`"
+      />
+    </RouterLink>
     <div class="product-info">
-      <h2 data-wavy>{{ product.name }}</h2>
-      <h3 class="product-price">€{{ product.price.toFixed(2) }}</h3>
+      <RouterLink :to="`/product/${cartProduct.id}`" :key="cartProduct.id">
+        <h2 data-wavy :class="isHovered ? 'animate' : ''">{{ cartProduct.name }}</h2>
+        <h3 class="product-price">€{{ cartProduct.price.toFixed(2) }}</h3>
+      </RouterLink>
+      <button @click="addToCart" :disabled="cartProduct.stock === 0">
+        <CartPlusIcon />
+      </button>
     </div>
-  </RouterLink>
+  </div>
 </template>
 
 <script setup lang="ts">
 import type { Product } from '@/services/api/handlers'
 import ProductImage from '@/components/ProductImage.vue'
+import CartPlusIcon from '@/components/icons/CartPlusIcon.vue'
+import { ref } from 'vue'
 
-defineProps<{
+const props = defineProps<{
   product: Product
 }>()
+
+const isHovered = ref(false)
+const cartProduct = ref(props.product)
+
+const addToCart = async () => {
+  if (!props.product) return
+
+  try {
+    const response = await fetch('/api/cart', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ productId: props.product.id, quantity: 1 })
+    })
+
+    if (!response.ok) {
+      throw new Error("Couldn't add the product to the cart")
+    }
+
+    cartProduct.value = await response.json()
+  } catch (err) {
+    console.error('Error adding the product to the cart:', err)
+  }
+}
 </script>
 
 <style scoped>
@@ -26,8 +64,13 @@ defineProps<{
 
 .product-info {
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+  flex-direction: row;
+  justify-content: space-between;
+
+  button {
+    align-self: center;
+    background-color: transparent;
+  }
 }
 
 h2,
@@ -44,7 +87,23 @@ h2 {
   font-size: 1.2rem;
 }
 
+.image-container {
+  position: relative;
+  padding-top: 100%;
+  overflow: hidden;
+}
+
 img {
+  position: absolute;
+  top: 0;
+  left: 0;
   width: 100%;
+  transform: skew(0deg) scale(1);
+  transition: all 0.7s cubic-bezier(0.23, 1, 0.32, 1);
+  will-change: transform;
+
+  &.zoom {
+    transform: skew(4deg) scale(1.15);
+  }
 }
 </style>
---
+++ src/components/icons/CartPlusIcon.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="size"
+    :height="size"
+    viewBox="0 0 24 24"
+    fill="none"
+  >
+    <path
+      d="M13 21h-2.068c-2.75 0-4.126 0-5.08-.76-1.301-1.046-1.39-3.167-1.651-4.652l-.593-3.375a1.93 1.93 0 0 0-1.09-1.4.484.484 0 0 1-.27-.335c-.17-.807-.622-1.931.39-2.377C2.867 8 3.18 8 3.804 8H7.5m4 0h8.696c.625 0 .937 0 1.166.1 1.012.447.56 1.57.39 2.378a.484.484 0 0 1-.27.335 1.988 1.988 0 0 0-.802.687M15 17.5h7M18.5 21v-7M6.5 11 10 3m5 0 2.5 5"
+      class="stroke-color"
+      stroke-width="2"
+      stroke-linecap="round"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  size?: number
+}
+
+withDefaults(defineProps<Props>(), {
+  size: 32
+})
+</script>
